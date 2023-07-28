faba874b3ddc8c69a2aec1671bb2a50eac3034ff
andriiserpun <andrewserpun2001@gmail.com>
2023-07-28 22:31:28 +0300

    first commit

--- lesson38/notes_app/notes_app/settings.py
+++ lesson38/notes_app/notes_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kookpb9ax^z)0(epw-*&j(q3&qw0dh8v4yrsrd1*(10%9@osod'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
